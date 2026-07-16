0431f7d34b1c7bbd9d1949c7ae3c6b35bc30c2cb
Raviuday Singh <raviudaysingh87@gmail.com>
2026-07-16 17:08:19 +0000

    comparisons in js

---
+++ js_basics/05_comparison.js
@@ -0,0 +1,9 @@
+console.log(2>1);
+
+//Good practice to compare the variables with same data
+
+// === this is strict check.this compares not only the value of the variables but also the data types of both the variables
+
+console.log("2" == 2);//true
+console.log("2" === 2);//false
+
