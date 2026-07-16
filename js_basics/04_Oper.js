0c25dd88fd62634be2ff3eb4e17b548de0a41d27
Raviuday Singh <raviudaysingh87@gmail.com>
2026-07-16 16:57:20 +0000

    operations in js

---
+++ js_basics/04_Oper.js
@@ -0,0 +1,23 @@
+let Value = 3;
+let negValue = -Value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2**3);//this means 2 raised to power 3
+console.log(10/2);
+console.log(7%3);//Remainder
+
+let str1 = "hello";
+let str2 = " Ravi";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);//agar string first me hai toh saara ans will be calculated as string
+console.log(1 + 2 + "2");//agar Number first me hai toh saara ans will be calculated as Number
+
+let v = 100;
+console.log(v++);//postfix
+console.log(++v);//prefix
