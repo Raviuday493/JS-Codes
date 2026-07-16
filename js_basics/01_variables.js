b55399cf33371da4f2f23068ad3341a8ef18c555
Raviuday Singh <raviudaysingh87@gmail.com>
2026-07-16 06:49:06 +0000

    second

---
+++ js_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "ravi@.com"
+let accountAge = 20
+var accountPass = "12345"
+accountCity = "Mohali"//never use this 
+let accountState;//when printed will show undefined
+
+
+// accountId = 2 // Not allowed bec const but we can change let var
+
+// nowadays we dont use var we use let bec of block scope in let
+
+
+console.log(accountId);
+
+console.table([accountEmail,accountPass,accountCity,accountState])
---
+++ js_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // this means treat all JS code as new version
+
+// alert(3+2);// we are using node.js not browser
+
+let name = "Ravi";
+let age = 20;
+let isLogged = true;
+
+//  data types => number(int),string.boolean,null,symbol
+// null is standalone value given to variables 
+// symbol=>unique elements
+
+//objects we will discuss in coming lec
+
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
