b3b4b0fefcfa4a23081c61f6c467f208df564221
Raviuday493 <raviudaysingh87@gmail.com>
2026-07-17 22:48:21 +0530

    strings in js

--- js_basics/07_string.js
+++ js_basics/07_string.js
@@ -1,3 +1,21 @@
 const name = "Ravi";
+const name2 = new String('Ravi');//2 ways to declare a string
 const repo = 40;
-console.log(name + repo);
+// console.log(name + repo); outdated  way of concatenation
+
+console.log(`Hi my name is  ${name} and my repo count is  ${repo}`);//this is called string interpolation
+// console.log(name[1]); 
+// console.log(name.charAt(2));
+// console.log(name.length);
+
+const newString = "     Ravi    ";
+//Trim cuts the whitespaces in the string
+// console.log(newString.trim());
+
+const url = "https://ravi%20.com";
+console.log(url.replace('%20','->'));
+
+
+
+ 
+
